d14138ff37419b2132e6ba5568790c7e7b7ccd5f
Andrei Dragomir <dragomirandy900@gmail.com>
2020-09-08 19:24:06 +0300

    secure

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -20,11 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'B&E)H@McQfTjWnZr4u7w!z%C*F-JaNdRgUkXp2s5v8y/A?D(G+KbPeShVmYq3t6w'
+
+# SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = ["adrw-eu.herokuapp.com", "adrw.eu", "www.adrw.eu", "127.0.0.1", "localhost"]
 
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     'about',
     'blog',
     'projects',
+    'sslserver',
 ]
 
 MIDDLEWARE = [
@@ -147,6 +149,8 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT =True
 SECURE_HSTS_SECONDS=3600
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
 
 
 
