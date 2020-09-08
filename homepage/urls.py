9dbf7bd343ec9490639b4fba9180946e073b14bc
Andrei Dragomir <dragomirandy900@gmail.com>
2020-09-08 11:50:21 +0300

    verify cert

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,5 +3,5 @@ from homepage import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('.well-known/acme-challenge/muPYUH_8KW7eAFCZWePd2GOIDMUpVqCuiW3OySfES7s', views.certificate, name='certificate'),
+    path('.well-known/acme-challenge/g4cmTro7oBBmSUUbe4Y9VT42s3nShL1Cs9QJF0T4Fm4', views.certificate, name='certificate'),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -7,4 +7,4 @@ def homepage(request):
     return render(request, 'homepage/homepage.html', {})
 
 def certificate(request):
-    return HttpResponse("muPYUH_8KW7eAFCZWePd2GOIDMUpVqCuiW3OySfES7s.a2-S4sebDylkm7AjvFbUUnyTLPiz0Fnx4l0nshSa0YQ")
+    return HttpResponse("g4cmTro7oBBmSUUbe4Y9VT42s3nShL1Cs9QJF0T4Fm4.a2-S4sebDylkm7AjvFbUUnyTLPiz0Fnx4l0nshSa0YQ")
