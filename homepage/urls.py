0e3df130f52f66c56cc3125278f83e84ed2b5cd2
Andrei Dragomir <dragomirandy900@gmail.com>
2020-09-08 11:21:56 +0300

    verify cert

--- homepage/urls.py
+++ homepage/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from homepage import views
 
 urlpatterns = [
-    path('', views.homepage, name='homepage')
+    path('', views.homepage, name='homepage'),
+    path('.well-known/acme-challenge/gaHo22OCDjGul00lQC68n-APKBhyw3vBBTuYyTQ-LFU', views.certificate, name='certificate'),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 def homepage(request):
     return render(request, 'homepage/homepage.html', {})
+
+def certificate(request):
+    return HttpResponse("gaHo22OCDjGul00lQC68n-APKBhyw3vBBTuYyTQ-LFU.a2-S4sebDylkm7AjvFbUUnyTLPiz0Fnx4l0nshSa0YQ")
